7e8c13fc82912e25789186a5493711cc9397559a
Boris Shimanovsky <boris@factual.com>
2015-01-29 15:10:32 -0800

    adding hostname and relying on environment variables

--- start_marathon.sh
+++ start_marathon.sh
@@ -3,5 +3,4 @@
 /usr/bin/java -Xmx512m \
 -Djava.library.path=/usr/local/lib \
 -Djava.util.logging.SimpleFormatter.format=%2$s%5$s%6$s%n \
--cp /usr/local/bin/marathon mesosphere.marathon.Main \
---zk $MARATHON_ZK --master $MESOS_ZK
+-cp /usr/local/bin/marathon mesosphere.marathon.Main
