5fc9ca62e788f6c45ce4eb1aa20fa457724b7356
Boris Shimanovsky <boris@factual.com>
2015-02-04 17:44:20 -0800

    putting hostname in explicitly

--- start_marathon.sh
+++ start_marathon.sh
@@ -4,4 +4,4 @@
 -Djava.library.path=/usr/local/lib \
 -Djava.util.logging.SimpleFormatter.format=%2$s%5$s%6$s%n \
 -cp /usr/local/bin/marathon mesosphere.marathon.Main \
---zk $MARATHON_ZK --master $MARATHON_MASTER
+--zk $MARATHON_ZK --master $MARATHON_MASTER --hostname $MARATHON_HOSTNAME
