eccc97287b66d715f1b15ed86561d9a06df13e28
Boris Shimanovsky <boris@factual.com>
2015-02-17 11:49:09 -0800

    bumped to version 8

--- start_marathon.sh
+++ start_marathon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /usr/bin/java -Xmx512m \
--Djava.library.path=/usr/local/lib \
+-Djava.library.path=/usr/local/lib:/usr/lib:/usr/lib64 \
 -Djava.util.logging.SimpleFormatter.format=%2$s%5$s%6$s%n \
--cp /usr/local/bin/marathon mesosphere.marathon.Main \
+-cp /usr/bin/marathon mesosphere.marathon.Main \
 --zk $MARATHON_ZK --master $MARATHON_MASTER --hostname $MARATHON_HOSTNAME
