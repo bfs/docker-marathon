bb8f03a597ee5bba6c51219237b9c7f89185147c
Daniel Ochoa <danielochoa@Daniels-MacBook-Pro.local>
2016-04-18 23:40:24 -0700

    Update start_marathon.sh, README.md - add mesos_role option to marathon command

--- start_marathon.sh
+++ start_marathon.sh
@@ -6,6 +6,7 @@ export MARATHON_HOSTNAME=${MARATHON_HOSTNAME:-${HOSTNAME:-`hostname`}}
 export MARATHON_HTTP_PORT=${MARATHON_HTTP_PORT:-${PORT:-8080}}
 export MARATHON_MASTER=${MARATHON_MASTER:-"zk://localhost:2181/mesos"}
 export MARATHON_ZK=${MARATHON_ZK:-"zk://localhost:2181/marathon"}
+export MARATHON_MESOS_ROLE=${MARATHON_MESOS_ROLE:-"*"}
 
 echo "-------Marathon Host ENV Variables-------"
 env
@@ -16,4 +17,4 @@ echo "----end Marathon Host ENV Variables----"
 -Djava.library.path=/usr/local/lib:/usr/lib:/usr/lib64 \
 -Djava.util.logging.SimpleFormatter.format=%2$s%5$s%6$s%n \
 -cp /usr/bin/marathon mesosphere.marathon.Main \
- --zk $MARATHON_ZK --master $MARATHON_MASTER --hostname $MARATHON_HOSTNAME --http_port $MARATHON_HTTP_PORT
+ --zk $MARATHON_ZK --master $MARATHON_MASTER --hostname $MARATHON_HOSTNAME --http_port $MARATHON_HTTP_PORT --mesos_role $MARATHON_MESOS_ROLE
